64a3d1c7dc011bf670aa4059e15ef1cd47daeb99
Rithvik Vibhu <rithvikvibhu@gmail.com>
2024-05-15 22:14:11 +0530

    revert dummy

--- src/cares_wrap.cc
+++ src/cares_wrap.cc
@@ -369,7 +369,6 @@ int ParseTlsaReply(Environment* env,
     void operator()(ares_dns_record_t* ptr) const noexcept {
       ares_dns_record_destroy(ptr);
     }
-
   };
   using ares_unique_ptr = std::unique_ptr<ares_dns_record_t[], AresDeleter>;
 
